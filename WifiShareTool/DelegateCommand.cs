0926c4323dd22ab9182b27fea739cafd77e28c91
CMF <1176023110@qq.com>
2019-09-03 01:35:31 +0800

    第一次提交

---
+++ WifiShareTool/DelegateCommand.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace WifiShareTool
+{
+    public class DelegateCommand<T> : ICommand
+    {
+        private readonly Action<T> _executeMethod = null;
+        private readonly Func<T, bool> _canExecuteMethod = null;
+
+        public DelegateCommand(Action<T> executeMethod)
+            : this(executeMethod, null)
+        {
+        }
+
+        public DelegateCommand(Action<T> executeMethod, Func<T, bool> canExecuteMethod)
+        {
+            if (executeMethod == null)
+                throw new ArgumentNullException("executeMetnod");
+            _executeMethod = executeMethod;
+            _canExecuteMethod = canExecuteMethod;
+        }
+
+        #region ICommand 成员
+
+        /// <summary>
+        ///  Method to determine if the command can be executed
+        /// </summary>
+        public bool CanExecute(T parameter)
+        {
+            if (_canExecuteMethod != null)
+            {
+                return _canExecuteMethod(parameter);
+            }
+
+            return true;
+
+        }
+
+        /// <summary>
+        ///  Execution of the command
+        /// </summary>
+        public void Execute(T parameter)
+        {
+            if (_executeMethod != null)
+            {
+                _executeMethod(parameter);
+            }
+        }
+
+        #endregion
+
+
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        #region ICommand 成员
+
+        public bool CanExecute(object parameter)
+        {
+            if (parameter == null && typeof(T).IsValueType)
+            {
+                return (_canExecuteMethod == null);
+
+            }
+
+            return CanExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            Execute((T)parameter);
+        }
+
+        #endregion
+    }
+
+    public class DelegateCommand : ICommand
+    {
+        private Action _executeMethod;
+        private Func<bool> _canExecuteMethod;
+
+        public DelegateCommand(Action executeMethod) : this(executeMethod, null)
+        {
+
+        }
+
+        public DelegateCommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            if (executeMethod == null)
+                throw new ArgumentNullException("executeMetnod");
+            _executeMethod = executeMethod;
+            _canExecuteMethod = canExecuteMethod;
+        }
+        public void Execute(object parameter)
+        {
+            _executeMethod?.Invoke();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecuteMethod != null)
+                return _canExecuteMethod();
+            else
+            {
+                return true;
+            }
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
